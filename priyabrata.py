8bfac794eb61ef2912907f564e815bd4e2d25e3a
priyabratamondal789-sys <priyabratamondal789@gmail.com>
2025-11-12 00:01:55 +0530

    Add files via upload

---
+++ priyabrata.py
@@ -0,0 +1,22 @@
+"""Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+addition=a+b
+print("addition of",a,"and",b,"is",addition)
+
+subtraction=a-b
+print("subtraction of",a,"and",b,"is",subtraction)
+
+multiplication=a*b
+print("multiplication of",a,"and",b,"is",multiplication)
+
+division=a/b
+print("division of",a,"and",b,"is",division)
