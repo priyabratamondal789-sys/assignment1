c83a844bfdcf8ca15715bb047500d5f41f434095
priyabratamondal789-sys <priyabratamondal789@gmail.com>
2025-11-12 00:35:39 +0530

    Add files via upload
    this file contains  the solution of a  problem statement which is written below
    
    Takes a user's first name and last name as input.
    2.  Concatenates the first name and last name into a full name.
    3.  Prints a personalized greeting message using the full name.

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+"""
+write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+
+a=input("Enter your first name:")
+b=input("Enter your last name:")
+
+c=a+b
+print("Good morning",c)
